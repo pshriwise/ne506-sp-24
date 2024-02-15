4db0273eb90364aa6537b5b86f1430983b8ab354
Patrick Shriwise <shriwise@tux-120.cae.wisc.edu>
2024-02-15 11:40:01 -0600

    Add option for CAE machine name

--- scripts/cae-openmc-jupyter.py
+++ scripts/cae-openmc-jupyter.py
@@ -5,16 +5,23 @@ from subprocess import run
 parser = ArgumentParser(description='Run OpenMC Jupyter Notebook')
 parser.add_argument('username', metavar='USERNAME', type=str,
                     help='CAE username')
-parser.add_argument('-d','--working-directory', metavar='WORKING_DIRECTORY', type=str,
+parser.add_argument('-c', '--cae-machine', metavar='CAE_MACHINE_NAME', type=str,
+                    help='CAE machine name', default='best-tux.cae.wisc.edu')
+parser.add_argument('-d', '--working-directory', metavar='WORKING_DIRECTORY', type=str,
                     help='working directory for the jupyter notebook', default='.')
 parser.add_argument('-p','--port', metavar='PORT', type=int,
                     help='port for the jupyter notebook', default=8888)
 
 args = parser.parse_args()
 
-cmd_template = 'ssh -t -L {}:localhost:{} {}@best-tux.cae.wisc.edu "/cae/apps/bin/cae-openmc \'jupyter-notebook --notebook-dir={} --no-browser --port={}\'"'
+cmd_template = 'ssh -t -L {}:localhost:{} {}@{} "/cae/apps/bin/cae-openmc \'jupyter-notebook --notebook-dir={} --no-browser --port={}\'"'
 
-cmd = cmd_template.format(args.port, args.port, args.username, args.working_directory, args.port)
+cmd = cmd_template.format(args.port,
+                          args.port,
+                          args.username,
+                          args.cae_machine,
+                          args.working_directory,
+                          args.port)
 
 print(f'Running command: {cmd}')
 
